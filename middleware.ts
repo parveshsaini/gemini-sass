bde9a2b22c0aa666511430cf60d4c104d5af4357
praxter11 <parveshsaini619@gmail.com>
2024-06-28 18:34:53 +0530

    fix: middleware bug fix

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,6 @@ export { default } from "next-auth/middleware"
 export const config= {
     matcher: [
         '/dashboard/history',
-        '/dashboard/content/:slug'
+        '/dashboard/content/:slug*'
     ]
 }
